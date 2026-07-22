7970382afeb303d891e8ce702bb1f02de95bd5a9
moecov-creator <moecov@gmail.com>
2026-07-21 19:04:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Demo from "./pages/Demo";
 import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
 import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
@@ -38,6 +39,7 @@ const App = () => (
             <Route path="/contact" element={<Contact />} />
             <Route path="/demo" element={<Demo />} />
             <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy" element={<Privacy />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
